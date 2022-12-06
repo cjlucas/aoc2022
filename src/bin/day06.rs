656fb11bbd5932247e3395eaf0c7a2767e01aee0
Chris Lucas <chris@cjl.io>
2022-12-06 13:17:28 -0500

    part2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -21,7 +21,20 @@ fn part1(input: &str) -> u64 {
 }
 
 fn part2(input: &str) -> u64 {
-    0
+    let mut answer = 0;
+
+    let chars: Vec<char> = input.chars().collect();
+
+    for window in chars.windows(14) {
+        let set: HashSet<&char> = window.iter().collect();
+        if set.len() == 14 {
+            return answer + 14;
+        }
+
+        answer += 1;
+    }
+
+    unreachable!()
 }
 
 fn main() {
@@ -36,42 +49,62 @@ mod tests {
     const SAMPLE_INPUT: &'static str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
 
     #[test]
-    fn test_day01_sample1() {
+    fn test_day01_part1_sample1() {
         assert_eq!(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
     }
 
     #[test]
-    fn test_day01_sample2() {
+    fn test_day01_part1_sample2() {
         assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
     }
 
     #[test]
-    fn test_day01_sample3() {
+    fn test_day01_part1_sample3() {
         assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
     }
 
     #[test]
-    fn test_day01_sample4() {
+    fn test_day01_part1_sample4() {
         assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
     }
 
     #[test]
-    fn test_day01_sample5() {
+    fn test_day01_part1_sample5() {
         assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
     }
 
     #[test]
-    fn test_day01() {
-        assert_eq!(part1(INPUT), 7766);
+    fn test_day01_part1() {
+        assert_eq!(part1(INPUT), 1896);
     }
 
-    // #[test]
-    // fn test_day02_sample() {
-    //     assert_eq!(part2(SAMPLE_INPUT), 70);
-    // }
+    #[test]
+    fn test_day01_part2_sample1() {
+        assert_eq!(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+    }
 
-    // #[test]
-    // fn test_day02() {
-    //     assert_eq!(part2(INPUT), 2415);
-    // }
+    #[test]
+    fn test_day01_part2_sample2() {
+        assert_eq!(part2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+    }
+
+    #[test]
+    fn test_day01_part2_sample3() {
+        assert_eq!(part2("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+    }
+
+    #[test]
+    fn test_day01_part2_sample4() {
+        assert_eq!(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+    }
+
+    #[test]
+    fn test_day01_part2_sample5() {
+        assert_eq!(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
+    }
+
+    #[test]
+    fn test_day01_part2() {
+        assert_eq!(part2(INPUT), 3452);
+    }
 }

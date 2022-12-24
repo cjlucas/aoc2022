49e446447e279ffe103f63e879d468ecfa338d8e
Chris Lucas <chris@cjl.io>
2022-12-24 00:57:42 -0500

    part2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -2,39 +2,22 @@ use std::collections::{HashMap, VecDeque};
 
 const INPUT: &'static str = include_str!("../../inputs/day12.txt");
 
-fn part1(input: &str) -> u64 {
-    let mut map: HashMap<(i64, i64), char> = HashMap::new();
+fn find_shortest_path(map: &HashMap<(i64, i64), char>, start: (i64, i64)) -> Option<u64> {
     let mut explored: HashMap<(i64, i64), (i64, i64)> = HashMap::new();
 
     let mut q = VecDeque::<(i64, i64)>::new();
-
-    let mut start = None;
-    for (y, line) in input.lines().enumerate() {
-        for (x, ch) in line.chars().enumerate() {
-            let p = (x as i64, y as i64);
-
-            if ch == 'S' {
-                start = Some(p);
-            }
-
-            map.insert(p, ch);
-        }
-    }
-
-    let start = start.unwrap();
 
     explored.insert(start, start);
     q.push_back(start);
 
     while let Some(point) = q.pop_front() {
         if map[&point] == 'E' {
-            println!("found end!");
             let mut answer = 1;
             let mut cur = point;
             loop {
                 let parent = explored[&cur];
-                if map[&parent] == 'S' {
-                    return answer;
+                if parent == start {
+                    return Some(answer);
                 }
 
                 cur = parent;
@@ -65,11 +48,49 @@ fn part1(input: &str) -> u64 {
         }
     }
 
-    unreachable!();
+    None
+}
+
+fn part1(input: &str) -> u64 {
+    let mut map: HashMap<(i64, i64), char> = HashMap::new();
+
+    let mut start = None;
+    for (y, line) in input.lines().enumerate() {
+        for (x, ch) in line.chars().enumerate() {
+            let p = (x as i64, y as i64);
+
+            if ch == 'S' {
+                start = Some(p);
+            }
+
+            map.insert(p, ch);
+        }
+    }
+
+    find_shortest_path(&map, start.unwrap()).unwrap()
 }
 
 fn part2(input: &str) -> u64 {
-    unreachable!()
+    let mut map: HashMap<(i64, i64), char> = HashMap::new();
+
+    let mut candidates = vec![];
+    for (y, line) in input.lines().enumerate() {
+        for (x, ch) in line.chars().enumerate() {
+            let p = (x as i64, y as i64);
+
+            if ch == 'S' || ch == 'a' {
+                candidates.push(p);
+            }
+
+            map.insert(p, ch);
+        }
+    }
+
+    candidates
+        .iter()
+        .filter_map(|start| find_shortest_path(&map, *start))
+        .min()
+        .unwrap()
 }
 
 fn main() {
@@ -93,13 +114,13 @@ mod tests {
         assert_eq!(part1(INPUT), 394);
     }
 
-    // #[test]
-    // fn test_day02_sample() {
-    //     assert_eq!(part2(SAMPLE_INPUT), 0);
-    // }
+    #[test]
+    fn test_day02_sample() {
+        assert_eq!(part2(SAMPLE_INPUT), 29);
+    }
 
-    // #[test]
-    // fn test_day02() {
-    //     assert_eq!(part2(INPUT), 0);
-    // }
+    #[test]
+    fn test_day02() {
+        assert_eq!(part2(INPUT), 388);
+    }
 }
